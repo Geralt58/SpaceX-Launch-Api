ca9f71aed915752ad8365c31a06cafc7846f67c6
Soumyadeep Basu <deep.basu5@gmail.com>
2022-05-21 20:50:06 +0530

    added animation to top button

--- src/Components/TopButton/TopButton.style.js
+++ src/Components/TopButton/TopButton.style.js
@@ -1,20 +1,23 @@
 import styled from 'styled-components'
 
 const ToTopDiv = styled.div`
-   position: fixed;
-   bottom: 50px;
-   right: 50px;
-
    .hide {
-      display: none;
+      bottom: -100px;
    }
-
    a {
+      position: fixed;
+      bottom: 50px;
+      right: 50px;
       display: inline-block;
       background: white;
       border-radius: 50%;
       padding: 1rem;
       box-shadow: 1px 1px 5px gray;
+      transition: background-color 0.5s ease-out, bottom 0.2s ease-in-out;
+
+      :hover {
+         background-color: #f2f2f2;
+      }
 
       img {
          max-width: 40px;
